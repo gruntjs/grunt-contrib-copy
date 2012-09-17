77c40dce79dbb619dd01ec779ba495fed7b75bbf
Chris Talkington <chris@christalkington.com>
2012-09-17 11:15:06 -0500

    follow nodeunit by using expected/actual.
    this should appease @tkellen.

--- test/copy_test.js
+++ test/copy_test.js
@@ -5,29 +5,29 @@ exports['copy'] = {
   main: function(test) {
     'use strict';
 
-    var expect, result;
+    var expected, actual;
 
     test.expect(5);
 
-    expect = fs.readdirSync('test/expected/copy_test_files').sort();
-    result = fs.readdirSync('tmp/copy_test_files').sort();
-    test.deepEqual(expect, result, 'should copy several files');
+    expected = fs.readdirSync('test/expected/copy_test_files').sort();
+    actual = fs.readdirSync('tmp/copy_test_files').sort();
+    test.deepEqual(expected, actual, 'should copy several files');
 
-    expect = fs.readdirSync('test/expected/copy_test_v0.1.0').sort();
-    result = fs.readdirSync('tmp/copy_test_v0.1.0').sort();
-    test.deepEqual(expect, result, 'should copy several folders and files (with template support)');
+    expected = fs.readdirSync('test/expected/copy_test_v0.1.0').sort();
+    actual = fs.readdirSync('tmp/copy_test_v0.1.0').sort();
+    test.deepEqual(expected, actual, 'should copy several folders and files (with template support)');
 
-    expect = fs.readdirSync('test/expected/copy_test_flatten').sort();
-    result = fs.readdirSync('tmp/copy_test_flatten').sort();
-    test.deepEqual(expect, result, 'should create a flat structure');
+    expected = fs.readdirSync('test/expected/copy_test_flatten').sort();
+    actual = fs.readdirSync('tmp/copy_test_flatten').sort();
+    test.deepEqual(expected, actual, 'should create a flat structure');
 
-    expect = fs.readdirSync('test/expected/copy_minimatch').sort();
-    result = fs.readdirSync('tmp/copy_minimatch').sort();
-    test.deepEqual(expect, result, 'should allow for minimatch dot option');
+    expected = fs.readdirSync('test/expected/copy_minimatch').sort();
+    actual = fs.readdirSync('tmp/copy_minimatch').sort();
+    test.deepEqual(expected, actual, 'should allow for minimatch dot option');
 
-    expect = grunt.file.read('test/expected/single.js');
-    result = grunt.file.read('tmp/single.js');
-    test.equal(expect, result, 'should allow for single file copy');
+    expected = grunt.file.read('test/expected/single.js');
+    actual = grunt.file.read('tmp/single.js');
+    test.equal(expected, actual, 'should allow for single file copy');
 
     test.done();
   }
